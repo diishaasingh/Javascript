666ada21535e98da4e707b1322f48e4db206b253
diishaasingh <104762434+diishaasingh@users.noreply.github.com>
2022-06-24 23:23:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+// console.log("hello world")
+// console.log("hello disha")
+// console.log(10+"20");
+//console.log(9-"5")
+//the above prgrm is a bug of javascript
+// var myName="Disha"//wew= are using camel case convention here for naming
+// var Name="Singh"
+// var myNumber=6376962236
+// console.log(typeof(myName))
+// console.log(typeof(myNumber))
+// console.log(myName+Name)
+// console.log(myName-Name)//this issues NaN(Not a Number)
+// console.log(true+true)
+// console.log(false-true)
+// console.log(false+false)
+
+//Difference between undefined and null
+// var iamUseless=null
+// var iamStandby
+// console.log(iamUseless)
+// console.log(iamStandby)
+// console.log(typeof(iamUseless))//this is the 2nd bug of js a sit shows the output of null as object whereas object is not among the 
+//6 primitive datatypes
+
+// var myNumber=6376962236
+// var myName="Disha"
+// console.log(isNaN(myNumber))
+// if(isNaN(myNumber)){
+//     console.log("plz enter a valid phone No.")
+// }
+// if(isNaN(myName)){
+//     console.log("plz enter a valid phone No.")
+// }
+
+// console.log(NaN===NaN)
+// console.log(Number.NaN===NaN) 
+// console.log(isNaN(NaN))
+// console.log(isNaN(Number.NaN))
+
+//expressions and operators
+console.log(5+20)
+
+
+
